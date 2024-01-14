590395ebfaa5f8f7be6f0038ab055f04c93bb63b
khanhUD <kn0958789@gmail.com>
2024-01-14 22:32:31 +0700

    lab1-2-3
    lab1-2-3

---
+++ Lab2/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'admin/lab2',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'db8b8c4cfa800650a64a2d0da6e0494801f20fff',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'admin/lab2' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'db8b8c4cfa800650a64a2d0da6e0494801f20fff',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
